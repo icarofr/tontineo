52ed9fe22fc1f84fed2ef03f01299a41d6626d17
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-30 12:06:02 +0200

    change controller tontine

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/create'
-  get 'user/update'
-  get 'user/edit'
-  get 'user/destroy'
-  get 'user/index'
-  get 'user/show'
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tontine
 end
