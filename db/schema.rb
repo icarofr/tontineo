a3192ea64a8aea81416483b7b7415bd6da41d0f0
jean-rodriguez kinouani <kinouani@gmail.com>
2022-08-30 16:46:21 +0200

    add nickname and install gem actioncable

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-x# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@ x# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_154847) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_141738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_154847) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
