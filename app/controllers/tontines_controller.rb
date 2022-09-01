250a4d4dbc6dc258c86a4132cf55aea9909af51d
Gorgui FAYE <gorgui92@msn.com>
2022-09-01 11:46:45 +0200

    view-new

--- app/controllers/tontines_controller.rb
+++ app/controllers/tontines_controller.rb
@@ -16,12 +16,13 @@ class TontinesController < ApplicationController
 	def create
 	  @tontine = Tontine.new(params_tontine)
 	  @tontine.user = current_user
+    @tontine.status = "pending"
 
 	  if @tontine.save!
  	    redirect_to edit_tontine_path(@tontine)
 	  else
 	  	render :new, status: :unprocessable_entity
-	  end 
+	  end
 	end
 
 	def edit
@@ -45,6 +46,6 @@ class TontinesController < ApplicationController
 	end
 
 	def params_tontine
-		params.require(:tontine).permit(:name, :contribution, :start_month, :payment_day, :participants, :status)
+		params.require(:tontine).permit(:name, :contribution, :start_month, :payment_day, :participants)
 	end
 end
