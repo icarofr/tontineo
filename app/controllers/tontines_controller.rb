88511cd75e026be3f04e253d362a067ae1bd6bf2
icarofr <icaro@gmx.fr>
2022-09-01 12:36:20 +0200

    fixed bug on tontine IDs (I don't trust Rails :/)

--- app/controllers/tontines_controller.rb
+++ app/controllers/tontines_controller.rb
@@ -1,51 +1,50 @@
 class TontinesController < ApplicationController
-	before_action :set_tontine, only: [:edit, :show, :destroy, :update]
+  before_action :set_tontine, only: %i[edit show destroy update]
 
-	def index
-	  @tontines = Tontine.all
-	end
+  def index
+    @tontines = Tontine.all
+  end
 
-	def show
+  def show
+  end
 
-	end
+  def new
+    @tontine = Tontine.new
+  end
 
-	def new
-	  @tontine = Tontine.new
-	end
-
-	def create
-	  @tontine = Tontine.new(params_tontine)
-	  @tontine.user = current_user
+  def create
+    @tontine = Tontine.new(params_tontine)
+    @tontine.id = Tontine.maximum(:id).next.to_i
+    @tontine.user = current_user
     @tontine.status = "pending"
 
-	  if @tontine.save!
- 	    redirect_to edit_tontine_path(@tontine)
-	  else
-	  	render :new, status: :unprocessable_entity
-	  end
-	end
+    if @tontine.save!
+      redirect_to edit_tontine_path(@tontine)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-	def edit
+  def edit
+  end
 
-	end
+  def update
+    @tontine.update(params_tontine)
+    redirect_to tontine_path(@tontine)
+  end
 
-	def update
-		@tontine.update(params_tontine)
-		redirect_to tontine_path(@tontine)
-	end
+  def destroy
+    @tontine.update(params_tontine)
+    redirect_to tontines_path, status: :see_other
+  end
 
-	def destroy
-	    @tontine.update(params_tontine)
-		redirect_to tontines_path, status: :see_other
-	end
+  private
 
-	private
+  def set_tontine
+    @tontine = Tontine.find(params[:id])
+  end
 
-	def set_tontine
-		@tontine = Tontine.find(params[:id])
-	end
-
-	def params_tontine
-		params.require(:tontine).permit(:name, :contribution, :start_month, :payment_day, :participants)
-	end
+  def params_tontine
+    params.require(:tontine).permit(:name, :contribution, :start_month, :payment_day, :participants)
+  end
 end
