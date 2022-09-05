8e1079073b4d5cd7071ef82853feedf84823209f
jean-rodriguez kinouani <kinouani@gmail.com>
2022-09-05 11:53:19 +0200

    fix calendar page tontine new

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static target = ['startMonthInput', 'paymentDayInputTarget']
+  static targets = ['paymentDayInput']
 
   connect() {
-    flatpickr(this.startMonthInputTarget)
+    console.log('hi from flatpickr');
+    console.log(this.paymentDayInputTarget);
+    flatpickr(this.paymentDayInputTarget)
   }
 }
