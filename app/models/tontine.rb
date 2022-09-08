27c4bc854c35694574cac3e103d6fac286eaf0d6
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2022-09-08 11:18:21 +0200

    show with chatroom button

--- app/models/tontine.rb
+++ app/models/tontine.rb
@@ -4,13 +4,24 @@ class Tontine < ApplicationRecord
   has_many :users, -> { order("members.position ASC") }, through: :members
   has_one_attached :cover
   
-  STATUS = %w[pending  active done]
+  STATUS_TRANSLATIONS = {
+    pending: "En attente",
+    active: "En cours",
+    done: "Terminée"
+  }.freeze
+
+  STATUS = STATUS_TRANSLATIONS.keys.freeze
+
   validates :status, inclusion: { in: Tontine::STATUS }
 
   after_create :create_first_member
 
   def current_position
     Date.today.month - self.start_month.month + 1
+  end
+
+  def status_fr
+    STATUS_TRANSLATIONS[status.to_sym]
   end
 
   private
